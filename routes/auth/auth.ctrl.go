4188da00775703db0ce0b3bd43a9099716a3e614
Viktor Patchev <viktor.patchev@inplayer.com>
2022-01-05 16:19:35 +0100

    Remove unnecessary routes

--- routes/auth/auth.ctrl.go
+++ routes/auth/auth.ctrl.go
@@ -7,16 +7,13 @@ import (
 	"github.com/bumi/lndhub.go/database/models"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/crypto/bcrypt"
 )
 
 // Register : Register Router
-func (AuthRouter) Register(c echo.Context) error {
+func (AuthRouter) Auth(c echo.Context) error {
 	type RequestBody struct {
 		Username string `json:"username" validate:"required"`
 		Password string `json:"password" validate:"required"`
-
-		DisplayName string `json:"display_name" validate:"required"`
 	}
 
 	var body RequestBody
@@ -35,11 +32,17 @@ func (AuthRouter) Register(c echo.Context) error {
 	}
 
 	user := models.User{
-		Username:     body.Username,
-		PasswordHash: body.Password,
-
-		DisplayName: body.DisplayName,
+		Model:        gorm.Model{},
+		Id:           0,
+		Email:        "",
+		Login:        "",
+		Password:     "",
+		RefreshToken: "",
+		AccessToken:  "",
+		UpdatedAt:    0,
+		CreatedAt:    0,
 	}
+
 
 	user.HashPassword()
 	db.Create(&user)
@@ -60,58 +63,3 @@ func (AuthRouter) Register(c echo.Context) error {
 	})
 }
 
-// Login : Login Router
-func (AuthRouter) Login(c echo.Context) error {
-	type RequestBody struct {
-		Username string `json:"username" validate:"required"`
-		Password string `json:"password" validate:"required"`
-	}
-
-	var body RequestBody
-
-	if err := c.Bind(&body); err != nil {
-		return c.NoContent(http.StatusNotFound)
-	}
-	if err := c.Validate(&body); err != nil {
-		return err
-	}
-
-	db, _ := c.Get("db").(*gorm.DB)
-
-	var user models.User
-
-	if err := db.Where("username = ?", body.Username).First(&user).Error; err != nil {
-		return c.NoContent(http.StatusConflict)
-	}
-
-	if bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(body.Password)) != nil {
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	token, _ := user.GenerateToken()
-
-	var cookie http.Cookie
-
-	cookie.Name = "token"
-	cookie.Value = token
-	cookie.Expires = time.Now().Add(7 * 24 * time.Hour)
-
-	c.SetCookie(&cookie)
-
-	return c.JSON(http.StatusOK, echo.Map{
-		"token": token,
-		"user":  user,
-	})
-}
-
-// Logout : Logout Router
-func (AuthRouter) Logout(c echo.Context) error {
-	tokenCookie, _ := c.Get("tokenCookie").(*http.Cookie)
-
-	tokenCookie.Value = ""
-	tokenCookie.Expires = time.Unix(0, 0)
-
-	c.SetCookie(tokenCookie)
-
-	return c.NoContent(200)
-}
--- routes/auth/auth.go
+++ routes/auth/auth.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"github.com/bumi/lndhub.go/lib/middlewares"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,7 +9,5 @@ type AuthRouter struct{}
 
 // Init : Init Router
 func (ctrl AuthRouter) Init(g *echo.Group) {
-	g.POST("/register", ctrl.Register)
-	g.POST("/login", ctrl.Login)
-	g.POST("/logout", ctrl.Logout, middlewares.Authoriszed)
+	g.POST("", ctrl.Auth)
 }
