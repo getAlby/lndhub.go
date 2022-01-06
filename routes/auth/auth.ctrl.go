00ccf96f762d611e6731d362e0266ab482424e7b
Viktor Patchev <viktor.patchev@inplayer.com>
2022-01-06 17:43:55 +0100

    User authentication logic

--- routes/auth/auth.ctrl.go
+++ routes/auth/auth.ctrl.go
@@ -2,8 +2,6 @@ package auth
 
 import (
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/bumi/lndhub.go/database/models"
 	"github.com/labstack/echo/v4"
@@ -13,47 +11,59 @@ import (
 // Register : Register Router
 func (AuthRouter) Auth(c echo.Context) error {
 	type RequestBody struct {
-		Id       int    `json:"id" validate:"required"`
-		Email    string `json:"email" validate:"required"`
-		Password string `json:"password" validate:"required"`
+		Login        string `json:"login"`
+		Password     string `json:"password"`
+		RefreshToken string `json:"refresh_token"`
 	}
 
 	var body RequestBody
-	var err error
-	body.Id, err = strconv.Atoi(c.FormValue("id"))
-	if err != nil {
-		return err
-	}
-	body.Email = c.FormValue("email")
+
+	body.Login = c.FormValue("login")
 	body.Password = c.FormValue("password")
+	body.RefreshToken = c.FormValue("refresh_token")
 
 	if err := c.Validate(&body); err != nil {
 		return err
+	}
+
+	if (body.Login == "" && body.Password == "") && body.RefreshToken == "" {
+		return c.JSON(http.StatusBadRequest, echo.Map{
+			"message": "login and password or refresh token is required",
+		})
 	}
 
 	db, _ := c.Get("db").(*gorm.DB)
 
 	var user models.User
 
-	if err := db.Where("id = ?", body.Id).First(&user).Error; err != nil {
-		return c.NoContent(http.StatusConflict)
+	if body.Login != "" && body.Password != "" {
+		if err := db.Where("login = ? AND password = ?", body.Login, body.Password).First(&user).Error; err != nil {
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "user not found",
+			})
+		}
+	} else if body.RefreshToken != "" {
+		if err := db.Where("refresh_token = ?", body.RefreshToken).First(&user).Error; err != nil {
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "user not found",
+			})
+		}
 	}
 
-	err = user.GenerateAccessToken(c, &user)
-	if err != nil {
-		return err
-	}
+	//err = user.GenerateAccessToken(c, &user)
+	//if err != nil {
+	//	return err
+	//}
 
-	var cookie http.Cookie
-
-	cookie.Name = "token"
-	cookie.Value = user.AccessToken
-	cookie.Expires = time.Now().Add(7 * 24 * time.Hour)
-
-	c.SetCookie(&cookie)
+	//var cookie http.Cookie
+	//
+	//cookie.Name = "token"
+	//cookie.Value = user.AccessToken
+	//cookie.Expires = time.Now().Add(7 * 24 * time.Hour)
+	//
+	//c.SetCookie(&cookie)
 
 	return c.JSON(http.StatusOK, echo.Map{
-		"token": user.AccessToken,
-		"user":  user,
+		"user": user,
 	})
 }
