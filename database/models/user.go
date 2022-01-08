47d8b704294f4909ccf5da96c1234904013bff6d
Viktor Patchev <viktor.patchev@inplayer.com>
2022-01-08 12:14:52 +0100

    Add tokens in separate directory

--- database/models/user.go
+++ database/models/user.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"github.com/dgrijalva/jwt-go"
-	"github.com/labstack/echo/v4"
 	"time"
 )
 
@@ -16,33 +14,4 @@ type User struct {
 	AccessToken  string
 	UpdatedAt    time.Time
 	CreatedAt    time.Time
-}
-
-// GenerateAccessToken : Generate Access Token
-func (u *User) GenerateAccessToken(c echo.Context) error {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": u.ID,
-		"email": u.Email,
-	})
-
-	t, err := token.SignedString([]byte("secret"))
-	if err != nil {
-		return err
-	}
-	u.AccessToken = t
-	return err
-}
-
-func (u *User) GenerateRefreshToken(c echo.Context) error {
-	refreshToken := jwt.New(jwt.SigningMethodHS256)
-	rtClaims := refreshToken.Claims.(jwt.MapClaims)
-	rtClaims["sub"] = u.ID
-
-	rt, err := refreshToken.SignedString([]byte("secret"))
-	if err != nil {
-		return err
-	}
-	u.RefreshToken = rt
-
-	return err
 }
---
+++ lib/tokens/jwt.go
@@ -0,0 +1,47 @@
+package tokens
+
+import (
+	"github.com/bumi/lndhub.go/database/models"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+)
+
+type jwtCustomClaims struct {
+	ID    uint   `json:"id"`
+	Email string `json:"email"`
+	Login string `json:"login"`
+
+	jwt.StandardClaims
+}
+
+// GenerateAccessToken : Generate Access Token
+func GenerateAccessToken(c echo.Context, u *models.User) error {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":    u.ID,
+		"email": u.Email,
+		"login": u.Login,
+	})
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+	u.AccessToken = t
+
+	return err
+}
+
+func GenerateRefreshToken(c echo.Context, u *models.User) error {
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	rtClaims := refreshToken.Claims.(jwt.MapClaims)
+	rtClaims["sub"] = u.ID
+
+	rt, err := refreshToken.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	u.RefreshToken = rt
+
+	return err
+}
