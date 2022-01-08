3a36a74d5c8e59d9c838db19437705fa4f546e05
Michael Bumann <hello@michaelbumann.com>
2022-01-08 17:55:14 +0100

    Update user DB config
    * use null string for optional attributes
    * auto update created_at/updated_at
    * set indexes

--- database/models/user.go
+++ database/models/user.go
@@ -1,17 +1,18 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 )
 
 // User : User Model
 type User struct {
 	ID           uint `gorm:"primary_key"`
-	Email        string
-	Login        string
-	Password     string
-	RefreshToken string
-	AccessToken  string
-	UpdatedAt    time.Time
-	CreatedAt    time.Time
+	Email        sql.NullString
+	Login        string         `gorm:"index"`
+	Password     string         `gorm:"index"`
+	RefreshToken sql.NullString `gorm:"index"`
+	AccessToken  sql.NullString `gorm:"index"`
+	UpdatedAt    time.Time      `gorm:"autoUpdateTime`
+	CreatedAt    time.Time      `gorm:"autoCreateTime"`
 }
