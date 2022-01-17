2e387132d1b5f649c8240244a2c55cd64856c53a
Michael Bumann <hello@michaelbumann.com>
2022-01-17 10:19:53 +0100

    Example

--- controllers/balance.ctrl.go
+++ controllers/balance.ctrl.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/bumi/lndhub.go/lib"
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,6 +12,8 @@ type BalanceController struct{}
 
 // Balance : Balance Controller
 func (BalanceController) Balance(c echo.Context) error {
+	ctx := c.(*lib.LndhubContext)
+	c.Logger().Warn(ctx.User.ID)
 	return c.JSON(http.StatusOK, echo.Map{
 		"BTC": echo.Map{
 			"AvailableBalance": 1,
