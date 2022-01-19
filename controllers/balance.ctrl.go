a6405fb6c481bbfbbd548442d335c89d74fea824
Michael Bumann <hello@michaelbumann.com>
2022-01-19 13:07:44 +0100

    Calculate user balance

--- controllers/balance.ctrl.go
+++ controllers/balance.ctrl.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/getAlby/lndhub.go/db/models"
 	"github.com/getAlby/lndhub.go/lib"
 	"github.com/labstack/echo/v4"
-	"github.com/lightningnetwork/lnd/lnrpc"
 )
 
 // BalanceController : BalanceController struct
@@ -16,16 +16,24 @@ type BalanceController struct{}
 func (BalanceController) Balance(c echo.Context) error {
 	ctx := c.(*lib.LndhubContext)
 	c.Logger().Warn(ctx.User.ID)
-	lndClient := *ctx.LndClient
-	getInfo, err := lndClient.GetInfo(context.TODO(), &lnrpc.GetInfoRequest{})
-	if err != nil {
-		panic(err)
+
+	db := ctx.DB
+
+	// load user's current account
+	account := models.Account{}
+	if err := db.NewSelect().Model(&account).Where("user_id = ? AND type= ?", ctx.User.ID, "current").Limit(1).Scan(context.TODO()); err != nil {
+		// TODO: proper error
+		return err
 	}
-	c.Logger().Infof("Connected to LND: %s - %s", getInfo.Alias, getInfo.IdentityPubkey)
+	// calculate the account balance
+	var balance int64
+	if err := db.NewSelect().Table("account_ledgers").ColumnExpr("sum(account_ledgers.amount) as balance").Where("account_ledgers.account_id = ?", account.ID).Scan(context.TODO(), &balance); err != nil {
+		return err
+	}
 
 	return c.JSON(http.StatusOK, echo.Map{
 		"BTC": echo.Map{
-			"AvailableBalance": 1,
+			"AvailableBalance": balance,
 		},
 	})
 }
