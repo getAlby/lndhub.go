ccc8ad8cd5507bf35f54d75d653183505a45f42e
Michael Bumann <hello@michaelbumann.com>
2022-01-19 11:46:10 +0100

    Comments

--- controllers/create.ctrl.go
+++ controllers/create.ctrl.go
@@ -22,6 +22,8 @@ type CreateUserController struct{}
 // CreateUser : Create user Controller
 func (CreateUserController) CreateUser(c echo.Context) error {
 	ctx := c.(*lib.LndhubContext)
+
+	// optional parameters that we currently do not use
 	type RequestBody struct {
 		PartnerID   string `json:"partnerid"`
 		AccountType string `json:"accounttype"`
@@ -36,11 +38,16 @@ func (CreateUserController) CreateUser(c echo.Context) error {
 
 	user := models.User{}
 
+	// generate user login/password (TODO: allow the user to choose a login/password?)
 	user.Login = randStringBytes(8)
 	password := randStringBytes(15)
+	// we only store the hashed password but return the initial plain text password in the HTTP response
 	hashedPassword := security.HashPassword(password)
 	user.Password = hashedPassword
 
+	// Create user and the user's accounts
+	// We use double-entry bookkeeping so we use 4 accounts: incoming, current, outgoing and fees
+	// Wrapping this in a transaction in case something fails
 	err := db.RunInTx(context.TODO(), &sql.TxOptions{}, func(ctx context.Context, tx bun.Tx) error {
 		if _, err := tx.NewInsert().Model(&user).Exec(ctx); err != nil {
 			return err
