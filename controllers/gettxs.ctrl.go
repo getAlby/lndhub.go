228edcbe39af84ba3e5154f83fa20990e0e6c794
Michael Bumann <hello@michaelbumann.com>
2022-01-21 11:49:08 +0100

    Return user invoices
    and maybe correctly encode the Buffers?

--- controllers/gettxs.ctrl.go
+++ controllers/gettxs.ctrl.go
@@ -1,17 +1,21 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/getAlby/lndhub.go/lib"
 	"github.com/getAlby/lndhub.go/lib/service"
 	"github.com/labstack/echo/v4"
 )
 
 // GetTXSController : GetTXSController struct
-type GetTXSController struct{}
+type GetTXSController struct {
+	svc *service.LndhubService
+}
 
 func NewGetTXSController(svc *service.LndhubService) *GetTXSController {
-	return &GetTXSController{}
+	return &GetTXSController{svc: svc}
 }
 
 // GetTXS : Get TXS Controller
@@ -21,6 +25,27 @@ func (controller *GetTXSController) GetTXS(c echo.Context) error {
 }
 
 func (controller *GetTXSController) GetUserInvoices(c echo.Context) error {
-	transactions := []string{}
-	return c.JSON(http.StatusOK, &transactions)
+	userId := c.Get("UserID").(int64)
+
+	invoices, err := controller.svc.InvoicesFor(context.TODO(), userId, "incoming")
+	if err != nil {
+		return err
+	}
+
+	response := make([]echo.Map, len(invoices))
+	for i, invoice := range invoices {
+		rhash, _ := lib.ToJavaScriptBuffer(invoice.RHash)
+		response[i] = echo.Map{
+			"r_hash":          rhash,
+			"payment_request": invoice.PaymentRequest,
+			"pay_req":         invoice.PaymentRequest,
+			"description":     invoice.Memo,
+			"payment_hash":    invoice.RHash,
+			"amt":             invoice.Amount,
+			"expire_time":     3600 * 24,
+			"timestamp":       invoice.CreatedAt.Unix(),
+			"type":            "user_invoice",
+		}
+	}
+	return c.JSON(http.StatusOK, &response)
 }
