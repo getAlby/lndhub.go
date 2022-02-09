1c14aa7f3122151b92981f9580cc56789f93be82
Stefan Kostic <skostic9242@gmail.com>
2022-02-09 21:58:15 +0100

    Add common package for constants

---
+++ common/globals.go
@@ -0,0 +1,12 @@
+package common
+
+const (
+	InvoiceTypeOutgoing = "outgoing"
+	InvoiceTypePaid     = "paid_invoice"
+	InvoiceTypeIncoming = "incoming"
+	InvoiceTypeUser     = "user_invoice"
+
+	InvoiceStateSettled     = "settled"
+	InvoiceStateInitialized = "initialized"
+	InvoiceStateOpen        = "open"
+)
