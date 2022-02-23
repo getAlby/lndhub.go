de823e478a53658c783a1d80db6ef911b7ddd8bf
Stefan Kostic <skostic9242@gmail.com>
2022-02-23 18:41:58 +0100

    Fix migration according to PR comments

--- db/migrations/20220115171101_init.up.sql
+++ db/migrations/20220115171101_init.up.sql
@@ -1,35 +1,16 @@
-CREATE TABLE public.accounts (
-    id bigint NOT NULL,
-    user_id bigint NOT NULL,
-    type character varying NOT NULL
+CREATE TABLE public.users (
+    id SERIAL PRIMARY KEY,
+    email character varying UNIQUE,
+    login character varying NOT NULL UNIQUE,
+    password character varying NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp with time zone
 );
 
 --bun:split
 
-CREATE SEQUENCE public.accounts_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
---bun:split
-
-ALTER SEQUENCE public.accounts_id_seq OWNED BY public.accounts.id;
-
---bun:split
-
-ALTER TABLE ONLY public.accounts ALTER COLUMN id SET DEFAULT nextval('public.accounts_id_seq'::regclass);
-
---bun:split
-
-ALTER TABLE ONLY public.accounts
-    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
-
---bun:split
-
 CREATE TABLE public.invoices (
-    id bigint NOT NULL,
+    id SERIAL PRIMARY KEY,
     type character varying,
     user_id bigint,
     amount bigint,
@@ -46,105 +27,46 @@ CREATE TABLE public.invoices (
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     expires_at timestamp with time zone,
     updated_at timestamp with time zone,
-    settled_at timestamp with time zone
+    settled_at timestamp with time zone,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)
+        ON DELETE CASCADE
 );
 
 --bun:split
 
-CREATE SEQUENCE public.invoices_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
---bun:split
-
-ALTER SEQUENCE public.invoices_id_seq OWNED BY public.invoices.id;
-
---bun:split
-
-ALTER TABLE ONLY public.invoices ALTER COLUMN id SET DEFAULT nextval('public.invoices_id_seq'::regclass);
-
---bun:split
-
-ALTER TABLE ONLY public.invoices
-    ADD CONSTRAINT invoices_pkey PRIMARY KEY (id);
+CREATE TABLE public.accounts (
+    id SERIAL PRIMARY KEY,
+    user_id bigint NOT NULL,
+    type character varying NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
 
 --bun:split
 
 CREATE TABLE public.transaction_entries (
-    id bigint NOT NULL,
+    id SERIAL PRIMARY KEY,
     user_id bigint NOT NULL,
     invoice_id bigint NOT NULL,
     parent_id bigint,
     credit_account_id bigint NOT NULL,
     debit_account_id bigint NOT NULL,
     amount bigint NOT NULL,
-    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
---bun:split
-
-CREATE SEQUENCE public.transaction_entries_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
---bun:split
-
-ALTER SEQUENCE public.transaction_entries_id_seq OWNED BY public.transaction_entries.id;
-
---bun:split
-
-ALTER TABLE ONLY public.transaction_entries ALTER COLUMN id SET DEFAULT nextval('public.transaction_entries_id_seq'::regclass);
-
---bun:split
-
-ALTER TABLE ONLY public.transaction_entries
-    ADD CONSTRAINT transaction_entries_pkey PRIMARY KEY (id);
-
---bun:split
-
-CREATE TABLE public.users (
-    id bigint NOT NULL,
-    email jsonb,
-    login character varying NOT NULL,
-    password character varying NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at timestamp with time zone
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id) 
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_credit_account
+        FOREIGN KEY(credit_account_id) 
+        REFERENCES accounts(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_debit_account
+        FOREIGN KEY(debit_account_id)
+        REFERENCES accounts(id)
+        ON DELETE CASCADE
 );
-
---bun:split
-
-CREATE SEQUENCE public.users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
---bun:split
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
---bun:split
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
---bun:split
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
---bun:split
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_login_key UNIQUE (login);
-
---bun:split
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
