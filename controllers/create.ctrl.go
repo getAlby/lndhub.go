7c28270c6aa4f8bf3fab8843cce051d1c64c13bb
Stefan Kostic <skostic9242@gmail.com>
2022-03-04 15:11:23 +0100

    Add proper err response to create ctrl

--- controllers/create.ctrl.go
+++ controllers/create.ctrl.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/getAlby/lndhub.go/lib/responses"
 	"github.com/getAlby/lndhub.go/lib/service"
 	"github.com/labstack/echo/v4"
 )
@@ -33,12 +34,13 @@ func (controller *CreateUserController) CreateUser(c echo.Context) error {
 	var body CreateUserRequestBody
 
 	if err := c.Bind(&body); err != nil {
-		return err
+		c.Logger().Errorf("Failed to load create user request body: %v", err)
+		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
 	}
 	user, err := controller.svc.CreateUser(c.Request().Context(), body.Login, body.Password)
-	//todo json response
 	if err != nil {
-		return err
+		c.Logger().Errorf("Failed to create user: %v", err)
+		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
 	}
 
 	var ResponseBody CreateUserResponseBody
