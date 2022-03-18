88d7d21aa103bde07ee514484c955bdedc3ca71b
Stefan Kostic <skostic9242@gmail.com>
2022-03-18 18:06:39 +0100

    Add common add invoice ctrl method

--- controllers/invoice.ctrl.go
+++ controllers/invoice.ctrl.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/getAlby/lndhub.go/lib/responses"
 	"github.com/getAlby/lndhub.go/lib/service"
-	"github.com/getsentry/sentry-go"
 	"github.com/labstack/echo/v4"
 )
 
@@ -26,34 +25,5 @@ func (controller *InvoiceController) Invoice(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
 	}
 
-	var body AddInvoiceRequestBody
-
-	if err := c.Bind(&body); err != nil {
-		c.Logger().Errorf("Failed to load invoice request body: %v", err)
-		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
-	}
-
-	if err := c.Validate(&body); err != nil {
-		c.Logger().Errorf("Invalid invoice request body: %v", err)
-		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
-	}
-
-	amount, err := controller.svc.ParseInt(body.Amount)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
-	}
-	c.Logger().Infof("Adding invoice: user_id=%v memo=%s value=%v description_hash=%s", user.ID, body.Memo, amount, body.DescriptionHash)
-
-	invoice, err := controller.svc.AddIncomingInvoice(c.Request().Context(), user.ID, amount, body.Memo, body.DescriptionHash)
-	if err != nil {
-		c.Logger().Errorf("Error creating invoice: %v", err)
-		sentry.CaptureException(err)
-		return c.JSON(http.StatusBadRequest, responses.BadArgumentsError)
-	}
-	responseBody := AddInvoiceResponseBody{}
-	responseBody.RHash = invoice.RHash
-	responseBody.PaymentRequest = invoice.PaymentRequest
-	responseBody.PayReq = invoice.PaymentRequest
-
-	return c.JSON(http.StatusOK, &responseBody)
+	return AddInvoice(c, controller.svc, user.ID)
 }
