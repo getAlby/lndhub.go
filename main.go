de5964913dd7d4f575f63cae0231d1d93d0e8467
juligasa <11684004+juligasa@users.noreply.github.com>
2022-06-29 14:01:03 +0200

    include more info on main error

--- main.go
+++ main.go
@@ -284,7 +284,7 @@ func main() {
 	// Start server
 	go func() {
 		if err := e.Start(fmt.Sprintf(":%v", c.Port)); err != nil && err != http.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server")
+			e.Logger.Fatal("shutting down the server. ", err.Error())
 		}
 	}()
 
