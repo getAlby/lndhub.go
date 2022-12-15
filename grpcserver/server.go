91df67d152ce5786097093cac45db47f3324851c
kiwiidb <kwinten.debacker@hotmail.com>
2022-12-15 11:23:24 +0100

    add support to listen from a certain id

--- grpcserver/server.go
+++ grpcserver/server.go
@@ -2,7 +2,6 @@ package grpcserver
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/getAlby/lndhub.go/common"
 	"github.com/getAlby/lndhub.go/db/models"
@@ -34,39 +33,63 @@ func NewGrpcServer(svc *service.LndhubService, ctx context.Context) (*Server, er
 }
 
 func (s *Server) SubsribeInvoices(req *lndhubrpc.SubsribeInvoicesRequest, srv lndhubrpc.InvoiceSubscription_SubsribeInvoicesServer) error {
+	alreadySeenId := int64(-1)
+	if req.FromId != nil {
+		//look up all settled incoming invoices from a certain id
+		//and return them first
+		invoices := []models.Invoice{}
+		err := s.svc.DB.NewSelect().Model(&invoices).Where("state = 'settled'").Where("type = 'incoming'").Where("id > ?", *req.FromId).Scan(s.ctx)
+		if err != nil {
+			return err
+		}
+		//add this so we can avoid duplicates in case of a race condition
+		if len(invoices) != 0 {
+			alreadySeenId = int64(invoices[len(invoices)-1].ID)
+		}
+		for _, inv := range invoices {
+			srv.Send(convertInvoice(inv))
+		}
+	}
 	for {
 		select {
 		case <-s.ctx.Done():
 			return nil
 		case inv := <-s.incomingInvoices:
-			customRecords := []*pb.Invoice_CustomRecords{}
-			for key, value := range inv.DestinationCustomRecords {
-				customRecords = append(customRecords, &pb.Invoice_CustomRecords{
-					//todo: fix types
-					Key:   strconv.Itoa(int(key)),
-					Value: string(value),
-				})
+			//in case we've already send it over
+			if inv.ID > alreadySeenId {
+				srv.Send(convertInvoice(inv))
 			}
-			srv.Send(&pb.Invoice{
-				Id:                   uint32(inv.ID),
-				Type:                 inv.Type,
-				UserId:               uint32(inv.UserID),
-				Amount:               uint32(inv.Amount),
-				Fee:                  uint32(inv.Fee),
-				Memo:                 inv.Memo,
-				DescriptionHash:      inv.DescriptionHash,
-				PaymentRequest:       inv.PaymentRequest,
-				DestinationPubkeyHex: inv.DestinationPubkeyHex,
-				CustomRecords:        customRecords,
-				RHash:                inv.RHash,
-				Preimage:             inv.Preimage,
-				Keysend:              inv.Keysend,
-				State:                inv.State,
-				CreatedAt:            timestamppb.New(inv.CreatedAt),
-				ExpiresAt:            timestamppb.New(inv.ExpiresAt.Time),
-				UpdatedAt:            timestamppb.New(inv.UpdatedAt.Time),
-				SettledAt:            timestamppb.New(inv.SettledAt.Time),
-			})
 		}
+	}
+}
+
+func convertInvoice(inv models.Invoice) *pb.Invoice {
+	customRecords := []*pb.Invoice_CustomRecords{}
+	for key, value := range inv.DestinationCustomRecords {
+		customRecords = append(customRecords, &pb.Invoice_CustomRecords{
+			//todo: fix types
+			Key:   key,
+			Value: value,
+		})
+	}
+	return &pb.Invoice{
+		Id:                   uint32(inv.ID),
+		Type:                 inv.Type,
+		UserId:               uint32(inv.UserID),
+		Amount:               uint32(inv.Amount),
+		Fee:                  uint32(inv.Fee),
+		Memo:                 inv.Memo,
+		DescriptionHash:      inv.DescriptionHash,
+		PaymentRequest:       inv.PaymentRequest,
+		DestinationPubkeyHex: inv.DestinationPubkeyHex,
+		CustomRecords:        customRecords,
+		RHash:                inv.RHash,
+		Preimage:             inv.Preimage,
+		Keysend:              inv.Keysend,
+		State:                inv.State,
+		CreatedAt:            timestamppb.New(inv.CreatedAt),
+		ExpiresAt:            timestamppb.New(inv.ExpiresAt.Time),
+		UpdatedAt:            timestamppb.New(inv.UpdatedAt.Time),
+		SettledAt:            timestamppb.New(inv.SettledAt.Time),
 	}
 }
