fa690530bb9c4990942c179227844d0e465ed3b9
Michael Bumann <hello@michaelbumann.com>
2022-12-26 22:28:17 +0100

    Create index concurrently

--- db/migrations/20221226203000_db_index.up.sql
+++ db/migrations/20221226203000_db_index.up.sql
@@ -1,53 +1,53 @@
-CREATE INDEX IF NOT EXISTS index_invoices_on_user_id ON invoices(user_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_user_id ON invoices(user_id);
 
 --migration:splitt
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_payment_request ON invoices(payment_request);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_payment_request ON invoices(payment_request);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_type ON invoices(type);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_type ON invoices(type);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_state ON invoices(state);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_state ON invoices(state);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_r_hash ON invoices(r_hash);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_r_hash ON invoices(r_hash);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_created_at ON invoices(created_at);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_created_at ON invoices(created_at);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_expires_at ON invoices(expires_at)
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_expires_at ON invoices(expires_at)
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_settled_at ON invoices(settled_at)
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_settled_at ON invoices(settled_at)
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_invoices_on_type_and_r_hash_and_state ON invoices(type, r_hash, state);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_type_and_r_hash_and_state ON invoices(type, r_hash, state);
 
 --migration:split
 
 
-CREATE INDEX IF NOT EXISTS index_transaction_entries_on_user_id ON transaction_entries(user_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_transaction_entries_on_user_id ON transaction_entries(user_id);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_transaction_entries_on_invoice_id ON transaction_entries(invoice_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_transaction_entries_on_invoice_id ON transaction_entries(invoice_id);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_transaction_entries_on_credit_account_id ON transaction_entries(credit_account_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_transaction_entries_on_credit_account_id ON transaction_entries(credit_account_id);
 
 --migration:split
 
-CREATE INDEX IF NOT EXISTS index_transaction_entries_on_debit_account_id ON transaction_entries(debit_account_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_transaction_entries_on_debit_account_id ON transaction_entries(debit_account_id);
 
 --migration:split
 
