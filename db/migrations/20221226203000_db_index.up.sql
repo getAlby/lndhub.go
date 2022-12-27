bad31df8befdd41c3efd9e53274d6f857733afef
Michael Bumann <hello@michaelbumann.com>
2022-12-27 10:17:47 +0100

    Too many index at once

--- db/migrations/20221226203000_db_index.up.sql
+++ db/migrations/20221226203000_db_index.up.sql
@@ -2,31 +2,8 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_user_id ON invoices(us
 
 --bun:split
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_payment_request ON invoices(payment_request);
-
---bun:split
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_type ON invoices(type);
-
---bun:split
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_state ON invoices(state);
-
---bun:split
-
 CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_r_hash ON invoices(r_hash);
 
---bun:split
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_created_at ON invoices(created_at);
-
---bun:split
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_expires_at ON invoices(expires_at)
-
---bun:split
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_invoices_on_settled_at ON invoices(settled_at)
 
 --bun:split
 
@@ -50,9 +27,4 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS index_transaction_entries_on_debit_accou
 
 --bun:split
 
-
 CREATE INDEX IF NOT EXISTS index_accounts_user_id ON accounts(user_id);
-
---bun:split
-
-CREATE INDEX IF NOT EXISTS index_accounts_type ON accounts(type);
