c17c29e94b4ee14dce9e9d19a71bf9bbbf31e196
Lucas Rouckhout <lucas.rouckhout@showpad.com>
2023-03-04 14:50:52 +0100

    Dummy test

--- integration_tests/lnd_rabbitmq_test.go
+++ integration_tests/lnd_rabbitmq_test.go
@@ -1,6 +1,8 @@
 package integration_tests
 
 import (
+	"github.com/getAlby/lndhub.go/lib/service"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -8,6 +10,24 @@ import (
 
 type LndRabbitMQSuite struct {
 	TestSuite
+	service *service.LndhubService
+}
+
+func (suite *LndRabbitMQSuite) SetupSuite() {
+
+	mockLnd := newDefaultMockLND()
+
+	svc, err := LndHubTestServiceInit(mockLnd)
+	if assert.Error(suite.T(), err) {
+		suite.T().Fail()
+	}
+
+	suite.service = svc
+}
+
+func (suite *LndRabbitMQSuite) TestDummy() {
+	assert.NotNil(suite.T(), suite.service.Config.RabbitMQUri)
+	suite.T().Log(suite.service.Config.RabbitMQUri)
 }
 
 func TestLndRabbitMQSuite(t *testing.T) {
