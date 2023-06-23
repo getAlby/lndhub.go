3bde54195f104c909d98f123fcda664a80621d43
kiwiidb <kwinten.debacker@hotmail.com>
2023-06-23 16:04:12 +0200

    move lnd init to it's own file

--- init_lnd.go
+++ init_lnd.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/getAlby/lndhub.go/lib/service"
 	"github.com/getAlby/lndhub.go/lnd"
 	"github.com/ziflex/lecho/v3"
 )
 
-func InitLNDCluster(c *service.Config, logger *lecho.Logger) (result lnd.LightningClientWrapper, err error) {
+func InitLNClient(c *service.Config, logger *lecho.Logger, ctx context.Context) (result lnd.LightningClientWrapper, err error) {
+	switch c.LNClientType {
+	case service.LND_CLIENT_TYPE:
+		return lnd.NewLNDclient(lnd.LNDoptions{
+			Address:      c.LNDAddress,
+			MacaroonFile: c.LNDMacaroonFile,
+			MacaroonHex:  c.LNDMacaroonHex,
+			CertFile:     c.LNDCertFile,
+			CertHex:      c.LNDCertHex,
+		}, ctx)
+	case service.LND_CLUSTER_CLIENT_TYPE:
+		return InitLNDCluster(c, logger, ctx)
+	default:
+		return nil, fmt.Errorf("Did not recognize LN client type %s", c.LNClientType)
+	}
+}
+func InitLNDCluster(c *service.Config, logger *lecho.Logger, ctx context.Context) (result lnd.LightningClientWrapper, err error) {
 	nodes := []*lnd.LNDWrapper{}
+	//interpret lnd address, macaroon file & cert file as comma seperated values
 	return &lnd.LNDCluster{
 		Nodes:               nodes,
 		ActiveChannelRatio:  0.5,
