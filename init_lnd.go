c4c925d282e64d45332c4bb1be9af545864f1c10
kiwiidb <kwinten.debacker@hotmail.com>
2023-06-23 16:08:13 +0200

    lnd cluster initialization loop

--- init_lnd.go
+++ init_lnd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/getAlby/lndhub.go/lib/service"
 	"github.com/getAlby/lndhub.go/lnd"
@@ -28,6 +29,23 @@ func InitLNClient(c *service.Config, logger *lecho.Logger, ctx context.Context) 
 func InitLNDCluster(c *service.Config, logger *lecho.Logger, ctx context.Context) (result lnd.LightningClientWrapper, err error) {
 	nodes := []*lnd.LNDWrapper{}
 	//interpret lnd address, macaroon file & cert file as comma seperated values
+	addresses := strings.Split(c.LNDAddress, ",")
+	macaroons := strings.Split(c.LNDMacaroonFile, ",")
+	certs := strings.Split(c.LNDCertFile, ",")
+	if len(addresses) != len(macaroons) || len(addresses) != len(certs) || len(certs) != len(macaroons) {
+		return nil, fmt.Errorf("Error parsing LND cluster config: addresses, macaroons or certs array length mismatch")
+	}
+	for i := 0; i < len(addresses); i++ {
+		n, err := lnd.NewLNDclient(lnd.LNDoptions{
+			Address:      addresses[i],
+			MacaroonFile: macaroons[i],
+			CertFile:     certs[i],
+		}, ctx)
+		if err != nil {
+			return nil, err
+		}
+		nodes = append(nodes, n)
+	}
 	return &lnd.LNDCluster{
 		Nodes:               nodes,
 		ActiveChannelRatio:  0.5,
