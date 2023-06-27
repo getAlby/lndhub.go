653a07130af14118d4a4f02fbc555074fe184d78
kiwiidb <kwinten.debacker@hotmail.com>
2023-06-27 11:57:34 +0200

    default liveness check 10s

--- lib/service/config.go
+++ lib/service/config.go
@@ -31,7 +31,7 @@ type Config struct {
 	LNDCertFile                      string  `envconfig:"LND_CERT_FILE"`
 	LNDMacaroonHex                   string  `envconfig:"LND_MACAROON_HEX"`
 	LNDCertHex                       string  `envconfig:"LND_CERT_HEX"`
-	LNDClusterLivenessPeriod         int     `envconfig:"LND_CLUSTER_LIVENESS_PERIOD" default:"30"`
+	LNDClusterLivenessPeriod         int     `envconfig:"LND_CLUSTER_LIVENESS_PERIOD" default:"10"`
 	LNDClusterActiveChannelRatio     float64 `envconfig:"LND_CLUSTER_ACTIVE_CHANNEL_RATIO" default:"0.5"`
 	CustomName                       string  `envconfig:"CUSTOM_NAME"`
 	Host                             string  `envconfig:"HOST" default:"localhost:3000"`
