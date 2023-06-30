9b17f35803250c14e9234a8fa417704b3a9eecda
Michael Bumann <hello@michaelbumann.com>
2023-06-30 13:12:59 +0200

    fix c&p error

--- lib/service/user.go
+++ lib/service/user.go
@@ -142,7 +142,7 @@ func (svc *LndhubService) CalcFeeLimit(destination string, amount int64) int64 {
 	if amount > 1000 {
 		limit = int64(math.Ceil(float64(amount)*float64(0.01)) + 1)
 	}
-	if amount > svc.Config.MaxFeeAmount {
+	if limit > svc.Config.MaxFeeAmount {
 		limit = svc.Config.MaxFeeAmount
 	}
 	return limit
