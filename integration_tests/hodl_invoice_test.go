205fe1a07795f358d00808d75fe14a2b825025f1
im-adithya <imadithyavardhan@gmail.com>
2023-12-12 19:18:25 +0530

    fix: add invoicesfor in tests

--- integration_tests/hodl_invoice_test.go
+++ integration_tests/hodl_invoice_test.go
@@ -152,7 +152,7 @@ func (suite *HodlInvoiceSuite) TestHodlInvoice() {
 	}
 	assert.Equal(suite.T(), int64(userFundingSats), userBalance)
 
-	invoices, err := suite.service.InvoicesFor(context.Background(), userId, common.InvoiceTypeOutgoing)
+	invoices, err := invoicesFor(suite.service, userId, common.InvoiceTypeOutgoing)
 	if err != nil {
 		fmt.Printf("Error when getting invoices %v\n", err.Error())
 	}
