342496471e7b1360050f281c0e5241ae211f91d4
Nicholas Sandomeno <nsando61@gmail.com>
2024-01-24 17:10:02 -0500

    add asset_id to invoice model

--- db/migrations/20220115171101_init.up.sql
+++ db/migrations/20220115171101_init.up.sql
@@ -5,33 +5,6 @@ CREATE TABLE users (
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone
 );
-
---bun:split
-
-CREATE TABLE invoices (
-    id SERIAL PRIMARY KEY,
-    type character varying,
-    user_id bigint,
-    amount bigint,
-    memo character varying,
-    description_hash character varying,
-    payment_request character varying,
-    destination_pubkey_hex character varying NOT NULL,
-    r_hash character varying,
-    preimage character varying,
-    internal boolean,
-    state character varying DEFAULT 'initialized',
-    error_message character varying,
-    add_index bigint,
-    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    expires_at timestamp with time zone,
-    updated_at timestamp with time zone,
-    settled_at timestamp with time zone,
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-);
 --bun:split
 CREATE TABLE assets (
     id SERIAL PRIMARY KEY,
@@ -50,6 +23,36 @@ CREATE TABLE accounts (
     user_id bigint NOT NULL,
     asset_id bigint NOT NULL,
     type character varying NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_asset
+        FOREIGN KEY(asset_id)
+        REFERENCES assets(id)
+        ON DELETE NO ACTION
+);
+--bun:split
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    type character varying,
+    user_id bigint,
+    asset_id bigint,
+    amount bigint,
+    memo character varying,
+    description_hash character varying,
+    payment_request character varying,
+    destination_pubkey_hex character varying NOT NULL,
+    r_hash character varying,
+    preimage character varying,
+    internal boolean,
+    state character varying DEFAULT 'initialized',
+    error_message character varying,
+    add_index bigint,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    expires_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    settled_at timestamp with time zone,
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
         REFERENCES users(id)
